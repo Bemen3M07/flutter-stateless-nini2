83776445fcc63856922f5a2c8627d87c218b9078
Nico Rios <niriga24@bemen3.cat>
2025-11-17 18:34:35 +0100

    exercici2.3

---
+++ lib/cat/bemen/m7/uf1/p2/exercici2.3/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  TextEditingController controller = TextEditingController();
+  int numeroSecreto = Random().nextInt(101); // 0 a 100
+  String mensaje = 'Adivina el número entre 0 y 100';
+
+  void comprobarNumero() {
+    int numeroUsuario = int.parse(controller.text);
+    
+    setState(() {
+      if (numeroUsuario < numeroSecreto) {
+        mensaje = 'El número que buscas es más grande';
+      } else if (numeroUsuario > numeroSecreto) {
+        mensaje = 'El número que buscas es más pequeño';
+      } else {
+        mensaje = 'Has acertado!';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(mensaje),
+              const SizedBox(height: 20),
+              TextField(
+                controller: controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Tu número',
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: comprobarNumero,
+                child: const Text('Comprobar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
